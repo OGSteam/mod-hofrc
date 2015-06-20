bbdf082fd4221704b8ea4a79e75f941d82863a72
Anthony <darknoon29@users.noreply.github.com>
2015-06-20 12:23:06 +0200

    Mise au propre du code source. (Indentations et alignements)

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * uninstall.php 
+ * uninstall.php
  * @package HofRC
  * @author Shad
  * @link http://www.ogsteam.fr
@@ -12,6 +12,6 @@ if (!defined('IN_SPYOGAME')) {
 global $db, $table_prefix;
 
 $mod_uninstall_name = "HOF_RC";
-$mod_uninstall_table = $table_prefix.'hofrc_config'.', '.$table_prefix."hofrc_skin".', '.$table_prefix."hofrc_attack".', '.$table_prefix."hofrc_defence".', '.$table_prefix."hofrc_info_rc".', '.$table_prefix."hofrc_rp".', '.$table_prefix."hofrc_title";
+$mod_uninstall_table = $table_prefix . 'hofrc_config' . ', ' . $table_prefix . "hofrc_skin" . ', ' . $table_prefix . "hofrc_attack" . ', ' . $table_prefix . "hofrc_defence" . ', ' . $table_prefix . "hofrc_info_rc" . ', ' . $table_prefix . "hofrc_rp" . ', ' . $table_prefix . "hofrc_title";
 uninstall_mod($mod_uninstall_name, $mod_uninstall_table);
 ?>
